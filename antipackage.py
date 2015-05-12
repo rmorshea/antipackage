704d11d1bf764c6e0e4666be52440f01d0af9c29
Ryan Morshead <ryan.morshead@gmail.com>
2015-05-11 18:33:49 -0700

    add utilities and streamline objs

--- antipackage.py
+++ antipackage.py
@@ -19,14 +19,54 @@ import shutil
 
 try:
     # python 2 import
-    from urllib import urlopen, urlretrieve
+    from urllib import urlretrieve
 except ImportError:
     # python 3 import
-    from urllib.requests import urlopen, urlretrieve
+    from urllib.requests import urlretrieve
 
 from IPython.utils.tempdir import TemporaryDirectory
 
 class RetrieveError(Exception): pass
+class GitHubError(Exception): pass
+
+# - - - - - - - - -
+# Utility Functions
+# - - - - - - - - -
+
+_conv = {}
+
+def replace_char(key, value=None, remove=False):
+    """Replace `key` with `value` in all import calls
+
+    Notes
+    -----
+    setting `remove` to True deletes a previously
+    created replacement rule assigned to `key`"""
+    if not remove:
+        if key != '.':
+            _conv[key] = value
+        else:
+            raise ValueError("'.' is a reserved character")
+    else:
+        del _conv[key]
+
+def repr_conv(chars):
+    """Applies replacements specified by `replace_char`"""
+    for key in _conv:
+        chars = chars.replace(key, _conv[key])
+    return chars
+
+def display_download(count, block, size):
+    """Displays download progress for sizable files
+
+    Notes
+    -----
+    Supply as the 'reporthook' for urlretrieve"""
+    fraction = size/block
+    if fraction>10:
+        percent = int(count*100/fraction)
+        sys.stdout.write("\rDownload in progress... %d%%" % percent)
+        sys.stdout.flush()
 
 # - - - - - - - - - - - - - - - -
 # Setup Base Dir and Pinning File
@@ -44,18 +84,19 @@ def _setup(directory, fname):
 
 _BASE_DIR, _DATA_PATH = _setup('.antipackage','pinnings.json')
 
-# - - - - - - - - - - - - - - - - - - - -
-# Main Data Management Objects, Functions
-# - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - -
+# Main Data Management Object and Functions
+# - - - - - - - - - - - - - - - - - - - - -
 
 def pin(path, **kwargs):
-    """Associate a repo a particular version
+    """Associate a repo with a particular version
 
     GitHub Paramters
     ----------------
     path : str
         A string indicating a data location via keys delimited
-        by a series of '/' characters.
+        by a series of '/' characters. This is an example of a
+        GitHub path - 'github/username/repo'.
     **kwargs : dict
         There should only be one keyword argument. That being
         for either 'branch', 'tag', or 'sha' corrisponding to
@@ -65,166 +106,127 @@ def pin(path, **kwargs):
         branch when importing. However marking a repo with a
         sha or tag pin will force antipackage to draw on
         version of the repository which orrisponds to that
-        particular commit during all future imports.
+        particular commit during all future imports. Default
+        points to 'master' branch of the repository indicated
+        by path.
     """
-    path = path.strip('/')
-    data = _Data(path)
-    key = path.split('/')[0]
-    pointer = Operand(key).use(path,**kwargs)
-    if data.items()!=pointer.items():
-        data.new = pointer
-        data.push()
+    Author(path,True).write(**kwargs)
 
 def data(path=None):
-    """Get pinning data at the given path
+    """Get a dictionary of pinning data at the given path
 
     GitHub Parameters
     -----------------
     path : str
         A string indicating a data location via keys delimited
-        by a series of '/' characters. This is an example of a
-        GitHub path - 'github/username/repo/commit/sha'. Given
-        this path, the function returns the sha associated with
-        that particular repository.
+        by a series of '/' characters.
     """
-    return dict(_Data(path))
+    with open(_DATA_PATH,'r') as f:
+        if not path:
+            return json.load(f)
+        else:
+            pathlist = path.strip('/').split('/')
+            data = json.load(f)
+            for k in pathlist:
+                data = data.get(k,dict())
+                if not data:
+                    return dict()
+            return data
 
-class Operand(object):
+class Author(object):
 
-    def __init__(self, key):
-        """Private function store
+    def __init__(self, path, save=True):
+        """Container holding private pinning formatters
 
         Parameters
         ----------
-        key : str
-            Idicates the name of a private function in Operand.
-            That function is assigned to self.use for public use.
+        path : str
+            A string indicating a data location via keys delimited
+            by a series of '/' characters. The first key of the path`
+            idicates the name of a private function in `self`. That
+            function is assigned to `self.write` for public use. For
+            example, the key 'github' sets up a formatter to write
+            GitHub pinning data.
+        save : bool
+            Default action causes pins writen with 'self.write' to be
+            pushed to the path and does not return them. If given as
+            'False' the formatter in 'self.write' returns the pin and
+            does not push them to the path.
         """
-        self.use = getattr(self, '_'+key)
+        self.path = path.strip('/')
+        self.pathlist = self.path.split('/')
+        try:
+            self.write = getattr(self, '_'+self.pathlist[0])
+        except AttributeError:
+            raise AttributeError("No pinning formatter for: '%s'" % self.pathlist[0])
+        self.save = save
+        self._new = None
 
-    def _github(self, path, **kwargs):
-        """Return a GitPointer object"""
+    def _github(self, **kwargs):
+        """Return or write GitHub pinning data"""
         if len(kwargs) == 0:
-            raise ValueError('No pin provided')
+            kwargs = {'branch':'master'}
         if len(kwargs) > 1:
             raise ValueError('Only one pin allowed per path')
-        repo = GitRepo(path.split('/'), *kwargs.items()[0])
-        return GitPointer(*repo.info)
-
-class _Data(dict):
-
-    def __init__(self, path=None, new=None):
-        """Object makes, gets and deletes pinning data
-
-        Paramters
-        ---------
-        path : str
-            A string indicating a data location via keys
-            delimited by a series of '/' characters. Default
-            returns all data.
-        new : dict
-            Optional argument which stores pinning data to
-            the given path after running `self.push()`
-        """
-        self.path = path
-        if not new:
-            data = self.pull()
-            if not data:
-                data = dict()
-        elif isinstance(new,dict):
-            self.new = new
-        super(_Data,self).__init__(data)
-
-    @property
-    def pathlist(self):
-        if self.path:
-            return self.path.strip('/').split('/')
+        name, value = kwargs.items()[0]
+        pathing = {'tag':'/tag', 'branch':'/branch', 'sha':'/commit/sha'}
+        for key, ext in pathing.items():
+            if name==key:
+                if value!=data(self.path+ext):
+                    repo = GitHubRepo(self.pathlist, name, value)
+                    self._new = {'commit':{'sha':repo.sha,'url':repo.url}}
+                    if name=='branch':
+                        self._new['branch'] = value
+                    elif name=='tag':
+                        self._new['tag'] = value
+                    self._new['id'] = 'pin'
+                if self._new and self.save:
+                    self.push()
+                else:
+                    return self._new or data(self.path)
         else:
-            return list()
-
-    def pull(self):
-        """Get pinning data from self.path"""
-        pathlist = self.pathlist
-        with open(_DATA_PATH,'r') as f:
-            if self.path is None:
-                return json.load(f)
-            else:
-                data = json.load(f)
-                for k in pathlist:
-                    data = data.get(k,None)
-                    if not data:
-                        return None
-                return data
-
+            if name not in ('branch','tag','sha'):
+                raise ValueError("Keywords must be 'branch',"
+                                " 'tag' or 'sha' not: '%s'" % name)
+        
     def push(self):
-        """Add self.new to pinning data"""
-        fname = _DATA_PATH
-        pathlist = self.pathlist
-
-        if os.path.exists(fname):
-            with open(fname,'r') as f:
+        """Write pinning data to file"""
+        try:
+            with open(_DATA_PATH,'r') as f:
                 data = json.load(f)
-        else:
-            raise RetrieveError('%s does not exist: call setup()' % fname)
+        except IOError:
+            raise IOError("%s does not exist: call '_setup()'" % fname)
 
         def push_new(data, index=0):
-            key = pathlist[index]
-            if index+1 == len(pathlist):
-                try:
-                    data[key] = self.new
-                except AttributeError:
+            key = self.pathlist[index]
+            if index+1 == len(self.pathlist):
+                if self._new:
+                    data[key] = self._new
+                else:
                     raise AttributeError('No data to push')
             else:
-                new = pathlist[index+1]
+                new = self.pathlist[index+1]
                 if not data.get(key,None):
                     data[key] = {new: None}
                 push_new(data[key],index+1)
             return data
 
-        with open(fname,'w') as f:
+        with open(_DATA_PATH,'w') as f:
             json.dump(push_new(data),f)
 
-    def _del(self):
-        """Delete self.path from the pinning data"""
-        pathlist = self.pathlist
-        err = "No data at '%s'" % '/'.join(pathlist)
-        with open(_DATA_PATH,'r+') as f:
-            raw = f.read()
-            if raw == str():
-                raise RetrieveError("'pinnings.json' is empty")
-            else:
-                data = json.loads(raw)
+# - - - - - - - - - - - - - - - - - -
+# Github Import Hook and Setup Object
+# - - - - - - - - - - - - - - - - - -
 
-            def pull_pin(data, index=0):
-                key = pathlist[index]
-                if index+1 == len(pathlist):
-                    if data[key].get('id',None) == 'pin':
-                        del data[key]
-                    else:
-                        raise RetrieveError('Not a pin')
-                else:
-                    new = pathlist[index+1]
-                    if not data.get(key,None):
-                        raise RetrieveError(err)
-                    pull_pin(data[key],index+1)
-                return data
-
-            json.dump(pull_pin(data),f)
-
-# - - - - - - - - - - - - - - - - - - - -
-# Github Setup, Download, Import Objects
-# - - - - - - - - - - - - - - - - - - - -
-
-class GitHandle(object):
-
-    base_dir = _BASE_DIR
+class GitHubHook(object):
 
     def __init__(self, fullname):
         """Handles imports from GitHub"""
-        sys.path.append(self.base_dir)
-        comps = fullname.split('.')
+        self.comps = fullname.split('.')
         fill = lambda c: c+[None for i in range(3-len(c))]
-        self.top, self.username, self.repo = fill(comps[:3])
+        index = [_BASE_DIR] + fill(self.comps[:3])
+        self.top, self.username, self.repo = index[1:]
+        self.index = [key for key in index if key]
         self.path = os.path.join(*self.index)
         self._setup_package()
 
@@ -236,7 +238,7 @@ class GitHandle(object):
             self._install_init()
 
     def _install_init(self):
-        if self.path != self.base_dir:
+        if self.path != _BASE_DIR:
             self._touch()
 
     def _touch(self):
@@ -245,20 +247,15 @@ class GitHandle(object):
         with open(path, 'a'):
             os.utime(path, None)
 
-    @property
-    def index(self):
-        raw = [self.base_dir, self.top, self.username, self.repo]
-        return [prop for prop in raw if prop]
-
     def waiting(self):
         """Ready for import?"""
-        if not self.repo:
+        if not self.repo or len(self.comps)>3:
             return True
         return False
 
     def exists(self):
         """Data and self.path exist?"""
-        self.data = _Data('/'.join(self.index[1:]))
+        self.data = data('/'.join(self.index[1:]))
         return os.path.exists(self.path) and self.data
 
     def update(self):
@@ -267,20 +264,20 @@ class GitHandle(object):
         reponame = '.'.join(pathlist)
         for key in self.data:
             if key in ('branch','tag'):
-                repo = GitRepo(pathlist, key, self.data[key])
+                repo = GitHubRepo(pathlist, key, self.data[key])
                 if repo.sha != self.data['commit']['sha']:
                     print('Updating repo: %s' % reponame)
                     self.data['commit']['sha'] = repo.sha
                     return True
-        print('Using existing version: %s' % reponame)
+        print('Using existing version of: %s' % reponame)
         return False
 
     def install(self):
         """Download and install new repository at self.path"""
         url = self.zip()
-        print('Downloading zip: %s' % url)
+        print("Downloading from: %s" % url)
         sys.stdout.flush()
-        temp_file, response = urlretrieve(url)
+        temp_file, response = urlretrieve(url, reporthook=display_download)
         zf = zipfile.ZipFile(temp_file)
         fname = zf.filename
         with TemporaryDirectory() as td:
@@ -300,15 +297,12 @@ class GitHandle(object):
         if self.data:
             src = self.data['commit']['sha']
         else:
-            repo = GitRepo(self.index[1:])
-            pin = GitPointer(*repo.info)
-            self.data.new = pin
-            self.data.push()
-            src = repo.value
+            pin('/'.join(self.index[1:]))
+            src = 'master'
         path.append(src)
         return 'https://api.github.com/'+'/'.join(path)
 
-class GitRepo(object):
+class GitHubRepo(object):
 
     host = {'top': 'https://api.github.com/repos/{1}/{2}/',
             'branch': 'branches/{0}',
@@ -332,62 +326,51 @@ class GitRepo(object):
             repository version being referenced"""
         self.name = name
         self.value = value
-        self.url = self._new_url(pathlist)
-        self.sha = self._new_sha()
+        self.url = self._url(pathlist)
+        self._git_init()
 
-    @property
-    def info(self):
-        return self.url, self.sha, self.name, self.value
-
-    def _new_url(self, pathlist):
-        """Format the url"""
+    def _url(self, pathlist):
+        """Format the GitHub url"""
         url = self.host['top'].format(*pathlist)
         url += self.host[self.name].format(self.value)
         return url
 
-    def _new_sha(self):
-        """Return the sha"""
+    def _git_init(self):
+        """Initialize data from GitHub"""
         temp_file = self.fetch()
+        with open(temp_file, 'r') as f:
+            data = json.load(f)
+            self.sha = self._sha(data)
+
+    def fetch(self):
+        """Retrieve data from GitHub"""
+        temp_file, response = urlretrieve(self.url)
+        if not response['Status'] == '200 OK':
+            e = 'at '+self.url
+            try:
+                with open(temp_file, 'r') as f:
+                    out = json.load(f)
+                e = "message "+e+" = '"+out['message']+"'"
+            except:
+                pass
+            raise GitHubError(e)
+        return temp_file
+
+    def _sha(self, data):
+        """Return the sha"""
         if self.name!='sha':
-            with open(temp_file, 'r') as f:
-                data = json.load(f)
-                if self.name == 'tag':
-                    data = self._handle_tag(data)
-                return data['commit']['sha']
+            if self.name == 'tag':
+                data = self._handle_tag(data)
+            return data['commit']['sha']
         else:
             return self.value
 
-    def fetch(self):
-        """Retrieve github data"""
-        temp_file, response = urlretrieve(self.url)
-        if not response['Status'] == '200 OK':
-            e = "Invalid url %s" % self.url
-            try:
-                with open(temp_file, 'r') as f:
-                    data = json.load(f)
-                e += " github message = '"+data['message']+"'"
-            except:
-                pass
-            raise RetrieveError(e)
-        return temp_file
-
     def _handle_tag(self, data):
-        """Handle tag data"""
+        """Retrieve data from a """
         for tag in data:
             if tag['name']==self.value:
                 return tag
         raise RetrieveError("No tag with name: '%s'" % value)
-
-class GitPointer(dict):
-
-    def __init__(self, url, sha, name, value=None):
-        """Returns formated GitHub pinning data"""
-        p = {'id':'pin', 'commit':{'sha':sha,'url':url}}
-        if name=='branch':
-            p['branch'] = value
-        elif name=='tag':
-            p['tag'] = value
-        super(GitPointer,self).__init__(p)
 
 # - - - - - - - - - - - -
 # Administrative Importer
@@ -395,21 +378,24 @@ class GitPointer(dict):
 
 class Importer(object):
 
-    github = GitHandle
-    required = True
+    github = GitHubHook
+
+    def __init__(self):
+        sys.path.append(_BASE_DIR)
+
+    def find_module(self, fullname, path=None):
+        fullname = repr_conv(fullname)
+        prefix = fullname.split('.')[0]
+        if hasattr(self, prefix):
+            source = getattr(self, prefix)(fullname)
+            if self.require(source):
+                source.install()
+                source.required = True
 
     def require(self, source):
         """Evaluates whether installation should occur"""
         if not source.waiting():
-            self.required = False
             return not source.exists() or source.update()
         return False
-
-    def find_module(self, fullname, path=None):
-        prefix = fullname.split('.')[0]
-        if self.required and hasattr(self, prefix):
-            source = getattr(self, prefix)(fullname)
-            if self.require(source):
-                source.install()
 
 sys.meta_path.insert(0, Importer())
